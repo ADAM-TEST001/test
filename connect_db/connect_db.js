6e2a5a5b314f52a6350d315b56bc82dae48dfa8d
ADAM-TEST001 <165468479+ADAM-TEST001@users.noreply.github.com>
2024-03-30 18:26:17 +0530

    Update connect_db.js

--- connect_db/connect_db.js
+++ connect_db/connect_db.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', true)
 
 
 const connect_db = () => {
-    return mongoose.connect(process.env.MONGO_DB_URL).then(() => {
+    return mongoose.connect('mongodb+srv://vercel-admin-user:qGH5gUiBpc3B,A:@test.scjs2vl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&appName=TEST').then(() => {
         console.log("DB CONNECTED");
     }).catch((err) => {
         console.log(err);
