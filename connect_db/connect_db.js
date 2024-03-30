f08afe45fd4f55e401dae5910a8790630c02d020
ADAM-TEST001 <165468479+ADAM-TEST001@users.noreply.github.com>
2024-03-30 17:33:29 +0530

    Update connect_db.js

--- connect_db/connect_db.js
+++ connect_db/connect_db.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', true)
 
 
 const connect_db = () => {
-    return mongoose.connect(process.env.MONGODB_URI).then(() => {
+    return mongoose.connect(process.env.MONGO_DB_URL).then(() => {
         console.log("DB CONNECTED");
     }).catch((err) => {
         console.log(err);
