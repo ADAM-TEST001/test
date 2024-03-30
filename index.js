b872cae5a5115100042c593b0263d2927ae3fccb
ADAM-TEST001 <165468479+ADAM-TEST001@users.noreply.github.com>
2024-03-30 17:50:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ const crypto = require('crypto');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.use(morgan("dev"));
 app.use(express.json());
