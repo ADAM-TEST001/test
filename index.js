8e2b41bd5575d5152976bfd4f6bb39a2dd44006c
ADAM-TEST001 <165468479+ADAM-TEST001@users.noreply.github.com>
2024-03-30 16:32:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const morgan = require('morgan')
 
 const app = express()
 
-app.use(cors({ origin: "*" }))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 
 app.use(morgan("dev"))
