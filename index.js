fd64b4b497100ae98c26fff400cf57929d88b0ac
ADAM-TEST001 <165468479+ADAM-TEST001@users.noreply.github.com>
2024-03-30 17:51:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.post('/register', async (req, res) => {
 
     // Save token in MongoDB
     try {
-        // await Token.create({ email, token });
+        await Token.create({ email, token });
         // Send verification email
         await sendVerificationEmail(email, token);
         res.json({ message: 'Verification email sent' });
