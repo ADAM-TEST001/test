f3d7c61f42f0b6e17a99f14311fefcb60f7f7fad
ADAM-TEST001 <165468479+ADAM-TEST001@users.noreply.github.com>
2024-03-30 19:07:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ async function sendVerificationEmail(email, token) {
 // Route to register a new user
 app.post('/register', async (req, res) => {
     const { email } = req.body.userDetails;
+     await connect_db();
 
     // Generate token
     const token = generateToken();
@@ -106,7 +107,6 @@ app.get('/verify', async (req, res) => {
 });
 
 app.listen(5000, async () => {
-     await connect_db();
      
     console.log("server is up");
 });
